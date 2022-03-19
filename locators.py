24843c55f95c41a50aa723a1e4986150dd113e71
Tatyana Kiseleva <kiseta@gmail.com>
2022-03-19 15:37:04 -0700

    added locators.py

---
+++ locators.py
@@ -0,0 +1,28 @@
+import random
+from datetime import datetime
+
+from faker import Faker
+fake = Faker(locale='en_US')
+
+# ------------------ DEMOBLAZE DATA ------------------------------
+app = 'DEMOBLAZE'
+base_url = 'https://www.demoblaze.com/index.html'
+product_url = 'https://www.demoblaze.com/prod.html?idp_='
+home_page_title = "STORE"
+full_name = f'{fake.first_name()} {fake.last_name()}'
+credit_card_number = fake.credit_card_number()
+username = full_name.replace(' ','').lower()
+password = fake.password()
+random_ids = random.sample(range(1, 15), 3)
+country = fake.country()
+city = fake.city()
+month = datetime.now().month
+year = datetime.now().year + 5
+product_list = []
+total = 0
+deleted_item_price = 0
+cart_total = 0
+new_cart_total = 0
+order_total = 0
+
+# -------------------------------------------------
